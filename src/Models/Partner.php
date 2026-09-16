9bb8270e28cf39d537601bad2c98d32115360dd5
Irineu Junior <irineu@fmd.ag>
2026-09-16 11:57:29 -0300

    refactor(filament-partners): adiciona atributo override em casts

--- src/Models/Partner.php
+++ src/Models/Partner.php
@@ -6,13 +6,13 @@ namespace Agenciafmd\Partners\Models;
 
 use Agenciafmd\Admix\Traits\WithScopes;
 use Agenciafmd\Partners\Database\Factories\PartnerFactory;
-use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Override;
 use OwenIt\Auditing\Auditable;
 use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 
@@ -37,6 +37,7 @@ final class Partner extends Model implements AuditableContract
             ->where('deleted_at', '<=', now()->subDays(30));
     }
 
+    #[Override]
     protected function casts(): array
     {
         return [
